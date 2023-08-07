34ace3f208d102e921edf89922469d5eb90a75fd
chris <104324755+cjurgens17@users.noreply.github.com>
2023-08-07 17:31:48 -0400

    Added getKey, getValue, hasKey, and hasValue methods for Reader class

--- jobs.js
+++ jobs.js
@@ -47,20 +47,21 @@ layers: {
 }
 }
 
-let oneLayer = {
-    name: 'chris'
-}
 
 let test = new Reader(data);
-test.print();
-let layers = test.layers();
-let max = test.deepestLayer();
-let test2 = new Reader(oneLayer);
-console.log(layers);
-console.log(max);
+// console.log(test.hasKey("nothing"));
+// console.log(test.hasKey("name"));
+// console.log(test.hasKey("seven"));
+// console.log(test.hasKey("number"));
+// console.log(test.hasKey("sex"));
+// console.log(test.hasKey("evenMore"));
+// console.log('values');
+// console.log(test.hasValue('name'));
+console.log(test.getKey('pokemon'));
+console.log(test.getKey(7));
+console.log(test.getKey('Developer'));
+console.log(test.getKey('male'));
 
-console.log(test2.deepestLayer());
-console.log(test2.layers());
 
 
 
