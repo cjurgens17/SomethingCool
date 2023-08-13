d549446528555f1e2d44851a5b197bae3b06407a
chris <104324755+cjurgens17@users.noreply.github.com>
2023-08-13 11:38:45 -0400

    This repo is linked in package.json for public registry of obeject-helper-reader. Finished up testing. made example js. changed an export

---
+++ example.js
@@ -0,0 +1,56 @@
+import Reader from 'object-helper-reader';
+
+let data = {
+name: 'chris',
+lastName:'Jurgens',
+age: 27,
+occupation: 'Developer',
+info: {
+    color: 'white',
+    height: 'tall',
+    moreInfo: {
+        sex: 'male',
+        guy: 'male',
+        ID: 1234,
+        evenMore: {
+            howMuch: 'a lot'
+        }
+      }
+},
+layers: {
+    more: 'info',
+    info: {
+        more: 'pokemon',
+        seven: {
+            number: 7
+        }
+    }
+}
+}
+
+//Example Data of methods
+let test = new Reader(data);
+console.log(test.hasKey("nothing"));
+console.log(test.hasKey("name"));
+console.log(test.hasKey("seven"));
+console.log(test.hasKey("number"));
+console.log(test.hasKey("sex"));
+console.log(test.hasKey("evenMore"));
+console.log('values');
+console.log(test.hasValue('name'));
+console.log(test.getKey('pokemon'));
+console.log(test.getKey(7));
+console.log(test.getKey('Developer'));
+console.log(test.getKey('male', ));
+console.log(test.getValue('more'));
+console.log(test.provide('layers'));
+console.log(test.deepestLayer());
+console.log(test.layers());
+test.print();
+
+
+
+
+
+
+
